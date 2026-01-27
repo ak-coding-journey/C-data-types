0e20e39ffd8642a411bc7992a06b192504ecd4fb
Arman Shaikh <shaikh.arman.work@gmail.com>
2026-01-27 19:24:26 +0530

    File upload
    Uploaded a C program demonstrating basic data types and formatted output using printf.

---
+++ data_types.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    printf("the integer value is %d.\n",a);
+
+    float b = 12.15;
+    printf("the float value is %.2f.\n",b);
+
+    char ch ='k';
+    printf("the character value is %c.\n",ch);
+
+
+    return 0;
+}
